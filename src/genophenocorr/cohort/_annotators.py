10951a5cd4465639f7819ac3a513ef0c9dea979b
Lauren Rekerle <102555796+lnrekerle@users.noreply.github.com>
2023-05-22 11:23:55 -0500

    Update _annotators.py
    Missed a spot when fixing merge conflicts

--- src/genophenocorr/cohort/_annotators.py
+++ src/genophenocorr/cohort/_annotators.py
@@ -29,7 +29,7 @@ class PhenopacketCohortCreator(CohortCreator):
             raise ValueError(f"No JSON Phenopackets were found in {phenopacket_directory}")
 
         cohort_variants, cohort_phenotypes, cohort_proteins, cohort_disease = set(), set(), set(), set()
-        for patient in cohort:
+        for patient in patients:
             cohort_variants.update(patient.variants)
             cohort_phenotypes.update(patient.phenotypes)
             cohort_proteins.update(patient.proteins)
