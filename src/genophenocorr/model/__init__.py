ceea6bbbb89285b505ab8c8b233901feae8a0520
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-09-26 13:44:37 -0400

    Reorder model elements in API reference.

--- src/genophenocorr/model/__init__.py
+++ src/genophenocorr/model/__init__.py
@@ -1,11 +1,16 @@
-from ._cohort import Patient, Cohort
+"""
+The `genophenocorr.model` package defines data model classes used in genophenocorr. We start with the top-level elements,
+such as :class:`Cohort` and :class:`Patient`, and we follow with data classes for phenotype, genotype, and protein info.
+"""
+
+from ._cohort import Cohort, Patient
 from ._phenotype import Phenotype
 from ._protein import FeatureInfo, FeatureType, ProteinFeature, ProteinMetadata
 from ._variant import VariantCoordinates, TranscriptAnnotation, Variant
 
 __all__ = [
+    'Cohort', 'Patient',
     'Phenotype',
-    'FeatureInfo', 'FeatureType', 'ProteinFeature', 'ProteinMetadata',
-    'VariantCoordinates', 'TranscriptAnnotation', 'Variant',
-    'Patient', 'Cohort'
+    'Variant', 'TranscriptAnnotation', 'VariantCoordinates',
+    'ProteinMetadata', 'ProteinFeature', 'FeatureInfo', 'FeatureType',
 ]
