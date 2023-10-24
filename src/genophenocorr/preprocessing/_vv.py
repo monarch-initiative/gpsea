d97f60acf4a9691d94503b84ed9d17472a543a6c
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-10-24 10:13:28 -0400

    Refactor `VVHgvsVariantCoordinateFinder`, remove debug print, improve documentation.

--- src/genophenocorr/preprocessing/_vv.py
+++ src/genophenocorr/preprocessing/_vv.py
@@ -6,13 +6,14 @@ import hpotk
 import requests
 
 from genophenocorr.model import VariantCoordinates, Genotype
-from genophenocorr.model.genome import GenomeBuild, GenomicRegion, Strand, GRCh38
-from ._api import VariantCoordinateFinder, T
+from genophenocorr.model.genome import GenomeBuild, GenomicRegion, Strand
+from ._api import VariantCoordinateFinder
 
 
 class VVHgvsVariantCoordinateFinder(VariantCoordinateFinder[typing.Tuple[str, str]]):
     """
-    `VVHgvsVariantCoordinateFinder` uses Variant Validator's REST API to build `VariantCoordinates` from an HGVS string.
+    `VVHgvsVariantCoordinateFinder` uses Variant Validator's REST API to build :class:`VariantCoordinates`
+    from an HGVS string.
 
     The finder takes a tuple with two strings:
 
@@ -21,16 +22,16 @@ class VVHgvsVariantCoordinateFinder(VariantCoordinateFinder[typing.Tuple[str, st
 
     and extracts the variant coordinates from the response.
 
-    URL: https://rest.variantvalidator.org/
+    :param genome_build: the genome build to use to construct :class:`VariantCoordinates`.
+    :param timeout: the REST API request timeout (default: 10).
     """
-    TIME_OUT = 10  # TODO @ielis: please set this to a value you find reasonable
-    select_options = ['all', 'raw', 'select', 'mane_select', 'mane', 'refseq_select']
-    SELECT = select_options[2]
 
-    def __init__(self, genome_build: GenomeBuild):
+    def __init__(self, genome_build: GenomeBuild, timeout: int = 10):
         self._build = hpotk.util.validate_instance(genome_build, GenomeBuild, 'genome_build')
+        self._timeout = timeout
         self._url = 'https://rest.variantvalidator.org/VariantValidator/variantvalidator/%s/%s/%s'
-        self.hgvs_pattern = re.compile(r'^NM_\d+\.\d+:c.\d+(_\d+)?.*')
+        self._headers = {'Content-type': 'application/json'}
+        self._hgvs_pattern = re.compile(r'^(?P<tx>NM_\d+\.\d+):c.\d+(_\d+)?.*')
 
     def find_coordinates(self, item: typing.Tuple[str, str]) -> typing.Tuple[VariantCoordinates, Genotype]:
         """
@@ -39,30 +40,28 @@ class VVHgvsVariantCoordinateFinder(VariantCoordinateFinder[typing.Tuple[str, st
         :return: variant coordinates and genotype
         """
         hgvs, genotype = item
-        transcript, _ = hgvs.split(':')
-        print(f"URL: {self._url}")
-        print(f"Values: {self._build.identifier_name}, {hgvs}")
-        request_url = self._url % (self._build.identifier_name, hgvs, transcript)
-        print(f"Request URL: {request_url}")
-        headers = {'Content-type': 'application/json'}
-        if self.hgvs_pattern.match(hgvs):
+        matcher = self._hgvs_pattern.match(hgvs)
+        if matcher:
+            transcript = matcher.group('tx')
+            request_url = self._url % (self._build.genome_build_id.major_assembly, hgvs, transcript)
+
             try:
-                response = requests.get(request_url, headers=headers, timeout=VVHgvsVariantCoordinateFinder.TIME_OUT)
+                response = requests.get(request_url, headers=self._headers, timeout=self._timeout)
                 response.raise_for_status()
                 response = response.json()
-                print(response)
-            except requests.exceptions.RequestException as e:
-                print(f"Error: {e}")
-
-            variant_coordinates = self._extract_variant_coordinates(response)
-
-            genotype = Genotype[genotype.upper()]
-            return variant_coordinates, genotype
+                variant_coordinates = self._extract_variant_coordinates(response)
+            except (requests.exceptions.RequestException, VariantValidatorDecodeException) as e:
+                raise ValueError(f'Error processing {hgvs}', e)
         else:
+            # The HGVS did not pass validation by a regular expression.
+            # Please submit an issue to our GitHub tracker if you believe
+            # the HGVS expression is correct.
             raise ValueError(f'Invalid HGVS string: {hgvs}')
 
-    def _extract_variant_coordinates(self, response: typing.Dict) \
-            -> typing.Optional[VariantCoordinates]:
+        genotype = Genotype[genotype.upper()]
+        return variant_coordinates, genotype
+
+    def _extract_variant_coordinates(self, response: typing.Dict) -> typing.Optional[VariantCoordinates]:
         """
         Extracts variant coordinates from a response from Variant Validator's REST API.
         :param response: Variant Validator's REST API response as a dictionary
@@ -73,27 +72,31 @@ class VVHgvsVariantCoordinateFinder(VariantCoordinateFinder[typing.Tuple[str, st
 
         selected_assembly = response['selected_assembly']
         variant_data = response['primary_assembly_loci'][selected_assembly.lower()]['vcf']
-        strand = Strand.POSITIVE
-        chrom = variant_data['chr']
+
+        contig = self._build.contig_by_name(variant_data['chr'])
+        if contig is None:
+            raise VariantValidatorDecodeException(f'Contig {variant_data["chr"]} was not found in build {self._build.identifier}')
+
         pos = int(variant_data['pos'])
         ref = variant_data['ref']
         alt = variant_data['alt']
-        start, end, change_length = self._extract_0based_mutation_range_from_1based_pos(pos, ref, alt)
 
-        variant_coordinates = VariantCoordinates(
+        start, end, change_length = self._extract_coordinates(pos, ref, alt)
+
+        return VariantCoordinates(
             region=GenomicRegion(
-                contig=self._build.contig_by_name(chrom),
+                contig=contig,
                 start=start,
                 end=end,
-                strand=strand,
+                strand=Strand.POSITIVE,
             ),
             ref=ref,
             alt=alt,
             change_length=change_length,
         )
-        return variant_coordinates
 
-    def _extract_0based_mutation_range_from_1based_pos(self, pos: int, ref: str, alt: str) -> typing.Tuple[int, int, int]:
+    @staticmethod
+    def _extract_coordinates(pos: int, ref: str, alt: str) -> typing.Tuple[int, int, int]:
         """
         Extracts 0-based start, end and change_length from 1-based position, reference and alternative alleles.
 
@@ -102,9 +105,15 @@ class VVHgvsVariantCoordinateFinder(VariantCoordinateFinder[typing.Tuple[str, st
         :param alt: alternative allele
         :return: 0-based start, end and change_length
         """
-        print(f'POS: {pos}, REF: {ref}, ALT: {alt}')
         change_length = len(alt) - len(ref)
         start = pos - 1
         end = start + len(ref)
 
         return start, end, change_length
+
+
+class VariantValidatorDecodeException(BaseException):
+    """
+    An exception thrown when parsing variant validator response fails.
+    """
+    pass
