4c24f08531caf024a5f0bded901906b379646ed5
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-10-25 17:18:02 -0400

    Disable the local test.

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,13 +1,16 @@
 import hpotk
 import pytest
 
+import pandas as pd
+
 from genophenocorr.analysis import CommunistCohortAnalysis
+from genophenocorr.analysis.predicate import BooleanPredicate
 from genophenocorr.data import get_toy_cohort
 from genophenocorr.model import Cohort, VariantEffect
 
 
-# @pytest.mark.skip(reason='Disabled unUnless explicitly enabled')
-class TestSlimCohortAnalysis:
+@pytest.mark.skip(reason='Disabled unless explicitly enabled')
+class TestCommunistCohortAnalysis:
 
     @pytest.fixture
     def toy_cohort(self) -> Cohort:
@@ -18,6 +21,8 @@ class TestSlimCohortAnalysis:
         return hpotk.load_minimal_ontology('')
 
     def test_compare_by_variant_effect(self, toy_cohort: Cohort, hpo: hpotk.MinimalOntology):
+        pd.set_option('expand_frame_repr', False)
         cohort_analysis = CommunistCohortAnalysis(toy_cohort, hpo)
         results = cohort_analysis.compare_by_variant_effect(VariantEffect.MISSENSE_VARIANT, 'NM_1234.5')
-        print(results)
+        summary = results.summarize(hpo, BooleanPredicate.YES)
+        print(summary)
