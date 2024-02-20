3adf5f28064a5abbf7283870f5a9d00b60c101f2
frehburg <fil.rehburg@gmail.com>
2024-02-20 15:44:36 +0100

    changing features to real data, currently not working due to codon indexing

--- src/genophenocorr/view/_draw_variants.py
+++ src/genophenocorr/view/_draw_variants.py
@@ -53,9 +53,9 @@ class VariantsVisualizer:
         self.exon_outline_color = 'black'
         self.axis_color = 'black'
 
-        self.feature_limits = [[56002271, 56002300], [56002600, 56002836]]
         self.exon_limits = [(cds.start, cds.end) for cds in self.tx_coordinates.get_cds_regions()]
         self.exon_labels = [f'{i + 1}' for i in range(len(self.exon_limits))]
+        self.feature_limits = [(feature.info.start, feature.info.end) for feature in self.protein_meta.protein_features]
         self.variant_locations = [56003221, 56004027, 56004027]
 
     def _draw_marker(self, x, min_y, max_y, circle_radius, color):
