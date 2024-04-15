97f408c7dff9feee69b2442c2c691db0aef6b9db
Lauren Rekerle <lauren.rekerle@jax.org>
2024-04-15 13:33:15 -0500

    fixed fisher tests

--- src/genophenocorr/analysis/_test_fisherExact.py
+++ src/genophenocorr/analysis/_test_fisherExact.py
@@ -9,14 +9,14 @@ def MultiExact() -> PythonMultiFisherExact:
     return PythonMultiFisherExact()
 
 @pytest.mark.parametrize('table, raise_error, pVal', 
-                        ([[[0,0], [0,0], [0,0]], pytest.raises(ValueError), None],
-                        [[[2,3], [1,0], [0,2]], does_not_raise(), 0.6429],
+                        ([[[0,0,0],[0,0,0]], pytest.raises(ValueError), None],
+                        [[[2, 1, 0],[3, 0, 2]], does_not_raise(), 0.6429],
                         #[[[100, 150], [500, 460], [420, 400]], pytest.raises(OverflowError), None],
-                        [[[5,5],[5,5],[5,5]], does_not_raise(), 1],
-                        [[[10,15], [5], [20,5]], pytest.raises(ValueError), None],
-                        [[[10, 1], [2,3], [3,4]], does_not_raise(), 0.0395],
-                        [[[6,6], [0,5], [0,8]], does_not_raise(), 0.0233],
-                        [[[],[],[]], pytest.raises(ValueError), None]
+                        [[[5,5,5],[5,5,5]], does_not_raise(), 1],
+                        [[[10, 5, 20], [15, 5]], pytest.raises(ValueError), None],
+                        [[[10, 2, 3],[1, 3, 4]], does_not_raise(), 0.0395],
+                        [[[6, 0, 0],[6, 5, 8]], does_not_raise(), 0.0233],
+                        [[[],[]], pytest.raises(ValueError), None]
 ))
 def test_multiFisherExact(table, raise_error, pVal, MultiExact):
     with raise_error:
