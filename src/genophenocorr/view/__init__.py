1aabf71d6466119b78c30d08e42860e979f2b2c3
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-05-08 14:04:40 -0400

    Remove the deleted `CohortViewer` from `genophenocorr.view`.

--- src/genophenocorr/view/__init__.py
+++ src/genophenocorr/view/__init__.py
@@ -1,9 +1,7 @@
 from ._cohort import CohortViewable
-from ._cohort import CohortViewer
 from ._txp import VariantTranscriptVisualizer
 
 __all__ = [
     'CohortViewable',
-    'CohortViewer',
     'VariantTranscriptVisualizer'
 ]
