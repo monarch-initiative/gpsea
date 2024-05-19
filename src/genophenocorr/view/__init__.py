ed5887d7ba10c90b8eb9baa6f8a762c3964fecf1
Peter Robinson <peter.robinson@jax.org>
2024-05-19 14:04:43 +0200

    protein drawing

--- src/genophenocorr/view/__init__.py
+++ src/genophenocorr/view/__init__.py
@@ -1,7 +1,10 @@
 from ._cohort import CohortViewable
+from ._protein_visualizable import ProteinVisualizable
 from ._txp import VariantTranscriptVisualizer
+from ._variant_drawer import VariantDrawer
 
 __all__ = [
     'CohortViewable',
+    'VariantDrawer',
     'VariantTranscriptVisualizer'
 ]
