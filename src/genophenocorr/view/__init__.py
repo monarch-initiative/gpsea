da66108bb1960f76f65fbbee41bd9f4d554c8b57
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-07-23 12:00:24 +0200

    We don't need to export `VariantFormatter` at this point.

--- src/genophenocorr/view/__init__.py
+++ src/genophenocorr/view/__init__.py
@@ -5,7 +5,6 @@ from ._protein_visualizable import ProteinVisualizable
 from ._stats import StatsViewer
 from ._txp import VariantTranscriptVisualizer
 from ._protein_visualizer import ProteinVisualizer
-from ._formatter import VariantFormatter
 
 __all__ = [
     'CohortViewable',
@@ -13,5 +12,4 @@ __all__ = [
     'DiseaseViewable',
     'StatsViewer',
     'VariantTranscriptVisualizer',
-    'VariantFormatter'
 ]
