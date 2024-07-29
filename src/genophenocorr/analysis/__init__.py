026e8d310cd2ec0944c674f55a01905ca9831874
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-07-29 10:59:44 +0200

    Add docs to MTC strategy enum, `terms_to_test` can be `None`, add tests.

--- src/genophenocorr/analysis/__init__.py
+++ src/genophenocorr/analysis/__init__.py
@@ -1,13 +1,13 @@
 from . import predicate
 
 from ._api import CohortAnalysis, GenotypePhenotypeAnalysisResult, HpoMtcReport
-from ._config import CohortAnalysisConfiguration, configure_cohort_analysis, configure_default_protein_metadata_service
+from ._config import CohortAnalysisConfiguration, configure_cohort_analysis, configure_default_protein_metadata_service, MTC_Strategy
 from ._gp_analysis import HeuristicMtcFilter, SpecifiedTermsMtcFilter, apply_predicates_on_patients
 
 __all__ = [
     'configure_cohort_analysis',
     'CohortAnalysis', 'GenotypePhenotypeAnalysisResult',
-    'CohortAnalysisConfiguration',
+    'CohortAnalysisConfiguration', 'MTC_Strategy',
     'HpoMtcReport',
     'HeuristicMtcFilter',
     'SpecifiedTermsMtcFilter',
