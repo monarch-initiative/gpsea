7ce3fa989961a5405e1dd28c37a8e805448cf4be
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-08-27 14:57:17 +0200

    Make release `0.2.0`.

--- src/gpsea/__init__.py
+++ src/gpsea/__init__.py
@@ -2,10 +2,4 @@
 GPSEA is a library for analyzing genotype-phenotype correlations in cohorts of rare disease patients.
 """
 
-from . import analysis
-from . import data
-from . import model
-from . import preprocessing
-from . import view
-
-__version__ = "0.1.1dev"
+__version__ = "0.2.0"
