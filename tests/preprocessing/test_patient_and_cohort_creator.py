4bc175289f25d79547a8a84b2995c2aa6fbe672e
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-08-29 20:53:31 +0200

    Minor test enhancements.

--- tests/preprocessing/test_patient_and_cohort_creator.py
+++ tests/preprocessing/test_patient_and_cohort_creator.py
@@ -96,10 +96,16 @@ class TestPhenopacketCohortCreator:
 
     def test_cohort_creator(
         self,
+        fpath_test_dir: str,
         phenopacket_cohort_creator: CohortCreator,
     ):
-        folder = os.path.join(os.getcwd(), 'tests', 'preprocessing', 'data', 'dup_id_test_data')
-        cohort, results = load_phenopacket_folder(folder, phenopacket_cohort_creator)
+        folder = os.path.join(fpath_test_dir, 'preprocessing', 'data', 'dup_id_test_data')
+        _, results = load_phenopacket_folder(folder, phenopacket_cohort_creator)
+        
         outfile = io.StringIO()
         results.summarize(outfile)
-        assert " Patient ID/s Pat_1[PMID_12345], Pat_2[PMID_67890] have a duplicate. Please verify every patient has an unique ID." in outfile.getvalue().split('\n')
+        
+        actual_lines = outfile.getvalue().split(os.linesep)
+
+        expected = " Patient ID/s Pat_1[PMID_12345], Pat_2[PMID_67890] have a duplicate. Please verify every patient has an unique ID."
+        assert expected in actual_lines
