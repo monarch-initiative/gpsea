381fd548e5c87bc2f4303430afacb8c390446ee2
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-09-09 21:33:55 +0200

    Refactor, rename to `CohortVariantViewer` and add a test.

--- src/gpsea/view/__init__.py
+++ src/gpsea/view/__init__.py
@@ -1,5 +1,5 @@
 from ._cohort import CohortViewable
-from ._all_variant_viewable import AllVariantViewable
+from ._cohort_variant_viewer import CohortVariantViewer
 from ._disease import DiseaseViewable
 from ._phenotype_analysis import summarize_hpo_analysis
 from ._protein_viewer import ProteinViewable
@@ -10,7 +10,7 @@ from ._protein_visualizer import ProteinVisualizer
 from ._formatter import VariantFormatter
 
 __all__ = [
-    'AllVariantViewable',
+    'CohortVariantViewer',
     'CohortViewable',
     'ProteinVisualizer', 'ProteinVisualizable', 'ProteinViewable',
     'DiseaseViewable',
--- src/gpsea/view/_cohort_variant_viewer.py
+++ src/gpsea/view/_cohort_variant_viewer.py
@@ -3,24 +3,25 @@ import typing
 from jinja2 import Environment, PackageLoader
 from collections import namedtuple, defaultdict
 
-from gpsea.model import Cohort
-from gpsea.model._variant import Variant, VariantEffect
+from gpsea.model import Cohort, Variant, VariantEffect
 from ._formatter import VariantFormatter
 
 
 ToDisplay = namedtuple('ToDisplay', ['hgvs_cdna', 'hgvsp', 'variant_effects'])
 
-VariantData = namedtuple('VariantData', ['variant_key', 'hgvs_cdna', 'hgvsp', 'variant_effects'] )
+VariantData = namedtuple('VariantData', ['variant_key', 'hgvs_cdna', 'hgvsp', 'variant_effects'])
 
-class AllVariantViewable:
+
+class CohortVariantViewer:
     """
-    Class to create a viewable object that is uses a Jinja2 template to create an HTML element
-    for display in the Jupyter notebook.
+    `AllVariantViewer` creates an HTML report with the cohort variants.
+
+    The report can be either written into an HTML file or displayed in a Jupyter notebook.
     """
 
     def __init__(
-            self,
-            transcript_id: str
+        self,
+        transcript_id: str
     ):
         """
         Args:
@@ -39,23 +40,22 @@ class AllVariantViewable:
         only_hgvs: bool = True
     ) -> str:
         """
-        Create an HTML that should be shown with display(HTML(..)) of the ipython package.
+        Create an HTML that should be shown with ``display(HTML(..))`` of the ipython package.
 
         Args:
-            cohort (Cohort): The cohort being analyzed in the current Notebook
-            transcript_id (str): the transcript that we map variants onto
+            cohort (Cohort): The cohort being analyzed in the current notebook.
+            only_hgvs (bool): Do not show the transcript ID part of the HGVS annotation, just the annotation.
 
         Returns:
             str: an HTML string with parameterized template for rendering
         """
-        context = self._prepare_context(cohort, transcript_id=self._transcript_id, only_hgvs=only_hgvs)
+        context = self._prepare_context(cohort, only_hgvs=only_hgvs)
         return self._cohort_template.render(context)
 
     def _prepare_context(
         self,
         cohort: Cohort,
-        transcript_id: typing.Optional[str],
-        only_hgvs
+        only_hgvs: bool,
     ) -> typing.Mapping[str, typing.Any]:
         variant_count_dictionary = defaultdict(int)
         variant_effect_count_dictionary = defaultdict(int)
@@ -74,9 +74,9 @@ class AllVariantViewable:
             variant_counts.append(
                 {
                     "variant_key": var_data.variant_key,
-                    "variant": var_data.hgvs_cdna, 
-                    "variant_name": var_data.hgvs_cdna, 
-                    "protein_name": var_data.hgvsp, 
+                    "variant": var_data.hgvs_cdna,
+                    "variant_name": var_data.hgvs_cdna,
+                    "protein_name": var_data.hgvsp,
                     "variant_effects": ", ".join(var_data.variant_effects),
                     "count": count,
                 }
@@ -88,7 +88,6 @@ class AllVariantViewable:
                     "count": count
                 }
             )
-        print(f"variants eee {len(variant_effect_counts)}")
         variant_counts = sorted(variant_counts, key=lambda row: row.get("count"), reverse=True)
         variant_effect_counts = sorted(variant_effect_counts, key=lambda row: row.get("count"), reverse=True)
 
@@ -100,7 +99,6 @@ class AllVariantViewable:
             "total_unique_allele_count": len(variant_counts)
         }
 
-
     def _get_variant_data(
         self,
         variant: Variant,
@@ -109,12 +107,11 @@ class AllVariantViewable:
         """
         Get user-friendly strings (e.g., HGVS for our target transcript) to match to the chromosomal strings
         Args:
-            cohort (Cohort): The cohort being analyzed in the current Notebook
-            transcript_id (str): the transcript that we map variants onto
+            variant (Variant): The variant to be formatted.
             only_hgvs (bool): do not show the transcript ID part of the HGVS annotation, just the annotation.
 
         Returns:
-            typing.Mapping[str, ToDisplay]: key: variant key, value: namedtuple(display (e.g. HGVS) string of variant, hgvsp protein string of variant)
+            VariantData: a named tuple with variant data formatted for human consumption.
         """
         variant_key = variant.variant_info.variant_key
         if variant.variant_info.has_sv_info():
@@ -122,7 +119,12 @@ class AllVariantViewable:
             gene_symbol = sv_info.gene_symbol
             display = f"SV involving {gene_symbol}"
             effect = VariantEffect.structural_so_id_to_display(so_term=sv_info.structural_type)
-            return VariantData(variant_key=variant_key, hgvs_cdna = display, hgvsp = "p.?", variant_effects=[effect])
+            return VariantData(
+                variant_key=variant_key,
+                hgvs_cdna=display,
+                hgvsp="p.?",
+                variant_effects=[effect],
+            )
         else:
             variant_key = variant.variant_info.variant_key
             display = self._var_formatter.format_as_string(variant)
@@ -136,13 +138,19 @@ class AllVariantViewable:
             if only_hgvs:
                 # do not show the transcript id
                 fields_dna = display.split(":")
-                fields_ps = hgvsp.split(":") if hgvsp is not None else [None]
                 if len(fields_dna) > 1:
                     display_hgvs_cDNA = fields_dna[1]
                 else:
                     display_hgvs_cDNA = fields_dna[0]
+                
+                fields_ps = hgvsp.split(":") if hgvsp is not None else (None,)
                 if len(fields_ps) > 1:
                     hgvsp = fields_ps[1]
                 else:
                     hgvsp = fields_ps[0]
-            return VariantData(variant_key=variant_key, hgvs_cdna = display_hgvs_cDNA, hgvsp = hgvsp, variant_effects=var_effects)
+            return VariantData(
+                variant_key=variant_key,
+                hgvs_cdna=display_hgvs_cDNA,
+                hgvsp=hgvsp,
+                variant_effects=var_effects,
+            )
