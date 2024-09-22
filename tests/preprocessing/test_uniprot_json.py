6472bd8595c3c83b2155ff52aceb8d98bba78241
Peter Robinson <peter.robinson@jax.org>
2024-09-22 12:26:52 +0200

    Fixing unit function signature of unit test

--- tests/preprocessing/test_uniprot_json.py
+++ tests/preprocessing/test_uniprot_json.py
@@ -17,26 +17,49 @@ class TestUniprotJsonToMetadata:
     """
 
     @pytest.fixture
-    def Q8IZT6_json_file_path(
+    def q8izt6_json_file_path(self,
         fpath_preprocessing_data_dir: str,
     ) -> str:
-        return os.path.join(fpath_preprocessing_data_dir, "Q8IZT6_manual_download.json")
+        return os.path.join(fpath_preprocessing_data_dir, "uniprot_response", "Q8IZT6_manual_download.json")
 
     @pytest.fixture
     def q8izt6_protein_metadata(self,
-                                Q8IZT6_json_file_path:str) -> ProteinMetadata:
+                                q8izt6_json_file_path:str) -> ProteinMetadata:
         """
         :returns: ProteinMetadata created from a downloaded UniProt JSON file
         """
         return ProteinMetadata.from_uniprot_json(
             protein_id=ITPR1_protein_id,
             label=ITPR1_protein_id,
-            uniprot_json=Q8IZT6_json_file_path,
+            uniprot_json=q8izt6_json_file_path,
             protein_length=ITPR1_protein_len,
         )
-    
-    def test_general_info(self, 
+
+    def test_general_info(self,
                           q8izt6_protein_metadata: ProteinMetadata):
+        """
+        grep -o location  Q8IZT6_manual_download.json | wc -l
+        47
+        Thus there are 47 Features in this JSON file.
+        """
         assert ITPR1_protein_len == q8izt6_protein_metadata.protein_length
         assert ITPR1_protein_id == q8izt6_protein_metadata.protein_id
-        assert len(q8izt6_protein_metadata.protein_features) == 2
+        assert len(q8izt6_protein_metadata.protein_features) == 47
+
+    def test_first_feature(self,
+                          q8izt6_protein_metadata: ProteinMetadata):
+        """
+        By inspection, the first feature is this:
+        {"type":"Domain",
+            "location":
+                {"start":{"value":920,"modifier":"EXACT"},
+                "end":{"value":1056,"modifier":"EXACT"}},
+            "description":"Calponin-homology (CH) 1",
+            "evidences":[{"evidenceCode":"ECO:0000255","source":"PROSITE-ProRule","id":"PRU00044"}]},
+        """
+        feature_0 = q8izt6_protein_metadata.protein_features[0]
+        assert feature_0.feature_type == FeatureType.DOMAIN
+        ## Note that we have converted to zero based internally, so this corresponds to 920 in the input
+        assert feature_0.info.start == 919
+        assert feature_0.info.end == 1056
+        assert feature_0.info.name == "Calponin-homology (CH) 1"
