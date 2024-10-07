311d7936809aacd0f8c55837678262ec9552519d
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-10-07 14:44:52 +0200

    The `nan` phenotype scores will be ignored.

--- src/gpsea/analysis/pscore/stats/_stats.py
+++ src/gpsea/analysis/pscore/stats/_stats.py
@@ -1,4 +1,5 @@
 import abc
+import math
 import typing
 
 from scipy.stats import mannwhitneyu, ttest_ind
@@ -25,6 +26,8 @@ class MannWhitneyStatistic(PhenotypeScoreStatistic):
     :func:`~scipy.stats.mannwhitneyu` function to apply
     Mann-Whitney U rank test on 2 phenotype scores.
 
+    The `NaN` phenotype score values are ignored.
+
     See :ref:`phenotype-score-stats` for an example usage.
     """
     
@@ -35,6 +38,8 @@ class MannWhitneyStatistic(PhenotypeScoreStatistic):
         assert len(scores) == 2, 'Mann-Whitney U rank test only supports 2 categories at this time'
         
         x, y = scores
+        x = MannWhitneyStatistic._remove_nans(x)
+        y = MannWhitneyStatistic._remove_nans(y)
         _, pval = mannwhitneyu(
             x=x,
             y=y,
@@ -43,13 +48,23 @@ class MannWhitneyStatistic(PhenotypeScoreStatistic):
 
         return pval
 
+    @staticmethod
+    def _remove_nans(
+        a: typing.Sequence[float],
+    ) -> typing.Sequence[float]:
+        return tuple(val for val in a if not math.isnan(val))
+
 
 class TTestStatistic(PhenotypeScoreStatistic):
     """
     `TTestStatistic` is a wrapper around SciPy's
     :func:`~scipy.stats.ttest_ind` function to apply
     T test on 2 phenotype scores.
+
+    The `NaN` phenotype score values are ignored.
     """
+
+    # TODO: refer to a user guide example to show a usage example.
 
     def compute_pval(
         self,
@@ -61,6 +76,7 @@ class TTestStatistic(PhenotypeScoreStatistic):
         res = ttest_ind(
             a=x, b=y,
             alternative='two-sided',
+            nan_policy="omit",
         )
 
         return res.pvalue
