a489017990ec4143874eda06fcc5b4e3b369e37e
Peter Robinson <peter.robinson@jax.org>
2024-10-14 14:58:36 +0200

    Fix box plot

--- src/gpsea/analysis/pscore/_api.py
+++ src/gpsea/analysis/pscore/_api.py
@@ -125,7 +125,8 @@ class PhenotypeScoreAnalysisResult:
         :param colors: a tuple with colors to use for coloring the box patches of the box plot.
         """
         # skip the patients with unassigned genotype group
-        not_na_gts = self._genotype_phenotype_scores["genotype"].notna()
+        bla = self._genotype_phenotype_scores.notna()
+        not_na_gts = bla.all(axis='columns')
         data = self._genotype_phenotype_scores.loc[not_na_gts]
         
         # Check that the provided genotype predicate defines the same categories
