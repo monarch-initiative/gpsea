16f4eaa3f10a932766bbdefd546dab717626939b
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-11-14 10:13:07 +0100

    Include CYP21A2 cohort in the tests.

--- tests/view/test_view.py
+++ tests/view/test_view.py
@@ -19,7 +19,7 @@ class TestCohortViewer:
             hpo=hpo,
         )
 
-    def test_process(
+    def test_process_suox_cohort(
         self,
         cohort_viewer: CohortViewer,
         suox_cohort: Cohort,
@@ -31,4 +31,18 @@ class TestCohortViewer:
         )
 
         with open(os.path.join("dev", "SUOX.cohort.html"), "w") as fh:
+            report.write(fh)
+
+    def test_process_cyp21a2_cohort(
+        self,
+        cohort_viewer: CohortViewer,
+        cyp21a2_cohort: Cohort,
+        cyp21a2_mane_tx_id: str,
+    ):
+        report = cohort_viewer.process(
+            cohort=cyp21a2_cohort,
+            transcript_id=cyp21a2_mane_tx_id,
+        )
+
+        with open(os.path.join("dev", "CYP21A2.cohort.html"), "w") as fh:
             report.write(fh)
